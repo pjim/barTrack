118860ec9a0bb44a8e1500ff0f71a79b6214a519
pjim <paul.k.james@googlemail.com>
2015-09-17 23:06:51 +0100

    added models with mongo login

---
+++ models/models.js
@@ -0,0 +1,18 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    provider: String;
+    uid: String;
+    name: String;
+    image: String;
+    created: {type: Date, default: Date.now}
+});
+
+var dbuser = process.env.DBUSER;
+var dbpassword = process.env.DBPASS;
+var connectString =   'mongodb://' + dbuser ':' + dbpassword + '@ds033097.mongolab.com:33097/bartrackbase'
+
+mongoose.connect(connectString);
+
+
