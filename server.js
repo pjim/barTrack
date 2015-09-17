b853f28dfc312a1064b2c825a2a94c27128778f4
pjim <paul.k.james@googlemail.com>
2015-09-17 14:32:19 +0100

    edit out credentials for heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var routes = require('./routes/routes.js');
 var app = express();
-var credentials = require('./credentials.js');
+//var credentials = require('./credentials.js');
 
 app.use(bodyParser.json());
 app.use(express.static('frontend/dist'));
