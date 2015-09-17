07ffa7122b904463fba25c3f026fab4791e710ec
pjim <paul.k.james@googlemail.com>
2015-09-17 17:42:08 +0100

    added passport and passport twitter into app

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var routes = require('./routes/routes.js');
 var app = express();
+var passport = require('passport');
 //var credentials = require('./credentials.js');
 
 app.use(bodyParser.json());
 app.use(express.static('frontend/dist'));
-
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/',routes);
 
 
