c7905163bcae28dbf855d3d03919dabfbdeb6187
pjim <paul.k.james@googlemail.com>
2015-09-17 13:42:18 +0100

    added logins as env

--- handlers.js
+++ handlers.js
@@ -1,13 +1,19 @@
 var credentials = require('./credentials.js');
 var RSVP = require('rsvp');
+var RSVP = require('rsvp');
 
-var yelp =require('yelp').createClient({
+/*var yelp =require('yelp').createClient({
      consumer_key:credentials.oAuth.consumerKey,
      consumer_secret:credentials.oAuth.consumerSecret,
      token:credentials.oAuth.token,
      token_secret:credentials.oAuth.tokenSecret
+});*/
+var yelp =require('yelp').createClient({
+     consumer_key:process.env.CONSUMER_KEY,
+     consumer_secret:process.env.CONSUMER_SECRET,
+     token:process.env.TOKEN,
+     token_secret:process.env.TOKEN_SECRET
 });
-
 
 module.exports.yelp = function(req,res){
 	var yelpJson;
